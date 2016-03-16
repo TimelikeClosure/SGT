7632d98c1c378ce1bdd38ca5dee11531dd8acaf5
Tim Horist Jr <Tim Horist Jr>
2016-03-16 16:06:59 -0700

    Switched test SELECT MySQL query to use prepared statement

--- server_requests/get.php
+++ server_requests/get.php
@@ -2,13 +2,37 @@
 
     require_once('connection.php');
 
+    $output = [
+        'data' => [],
+        'success' => null
+    ];
+
     if ($conn->connect_errno) {
-        echo "Failed to connect to database: " . $conn->connect_error;
+        echo "Failed to connect to database: {$conn->connect_errno}: {$conn->connect_error}";
     } else {
-        $query = "SELECT * FROM `grade_table`";
-        $result = $conn->query($query);
-        while ($row = $result->fetch_assoc()) {
-            print("<p>"); print_r($row); print("</p>");
+        $preparedStatement = $conn->prepare('SELECT course_name, grade, id, student_name FROM grade_table');
+        if (!$preparedStatement) {
+            print "Prepare failed: (" . $conn->errno . ") " . $conn->error;
         }
+        if (!$preparedStatement->execute()) {
+            print("Execute failed: ({$preparedStatement->errno}) {$preparedStatement->error}");
+        }
+        $result = [];
+        $preparedStatement->bind_result($result['course'], $result['grade'], $result['id'], $result['student']);
+        if (!$result){
+            print("Result failed: ({$preparedStatement->errno}) {$preparedStatement->error}");
+        }
+        while ($preparedStatement->fetch()) {
+            $temp['course'] = $result['course'];
+            $temp['grade'] = $result['grade'];
+            $temp['id'] = $result['id'];
+            $temp['student'] = $result['student'];
+            $output['data'][] = $temp;
+        }
+        $preparedStatement->close();
+        if ($output['success'] === null) {
+            $output['success'] = true;
+        }
+        print(json_encode($output));
     }
 ?>
