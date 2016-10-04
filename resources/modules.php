e2da624b1c423bedbd12e13765b69b9e2e6eac32
Tim Horist Jr <Tim Horist Jr>
2016-10-04 12:29:35 -0700

    added modules.php for keeping track of all used modules

---
+++ resources/modules.php
@@ -0,0 +1,12 @@
+<?php
+
+    //  Set module path
+    $modulePath = "modules/";
+
+    //  Require all modules
+    require_once($modulePath."libs/php/prepared.php");
+
+    //  Unset module path
+    unset($modulePath);
+
+?>
