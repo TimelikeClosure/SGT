e97fc78f1b9fb8b63ed33abf4d63063788ee1681
TimelikeClosure <thorist@gmail.com>
2016-10-17 17:41:57 -0700

    Increased dummy page id list to 15 records per page Removed grade record cache dummy data Removed record cache setTimeout dependancy Added record cache serverInterface dependancy gradeCache.recordList() now resolves record promises instantly if already cached and retrieves them from serverInterface if not Added serverInterface.getGradeById() method for getting individual grade record data from server

--- public/assets/js/services/grade_cache.js
+++ public/assets/js/services/grade_cache.js
@@ -1,47 +1,43 @@
-sgt.factory('gradeCache', ['$q', '$timeout', function($q, $timeout){
+sgt.factory('gradeCache', ['$q', 'serverInterface', function($q, serverInterface){
     function GradeCache(){
 
-        var grades = {
-            0: {
-                name: 'Angry Bob',
-                course: 'Anger Management',
-                grade: 32
-            },
-            1: {
-                name: 'Angry Bob',
-                course: 'Anger Expression',
-                grade: 87
-            },
-            2: {
-                name: 'Angry Bob',
-                course: 'Anger Substitution',
-                grade: 54
-            },
-            3: {
-                name: 'Calm Joe',
-                course: 'Anger Management',
-                grade: 87
-            },
-            4: {
-                name: 'Calm Joe',
-                course: 'Anger Expression',
-                grade: 31
-            },
-            5: {
-                name: 'Calm Joe',
-                course: 'Anger Substitution',
-                grade: 48
+        var grades = {};
+
+        function recordCached(id){
+            if (!grades.hasOwnProperty(id)){
+                return false;
             }
-        };
+            if (!grades[id].hasOwnProperty('name') || !grades[id].name){
+                return false;
+            }
+            if (!grades[id].hasOwnProperty('course') || !grades[id].course){
+                return false;
+            }
+            if (!grades[id].hasOwnProperty('grade') || typeof (grades[id].grade) != 'number'){
+                return false;
+            }
+            return true;
+        }
 
         this.recordList = function(idList){
             var recordListPromises = idList.map(function(id){
                 var recordPromise = $q.defer();
-
-                $timeout(function(){
+                if (recordCached(id)){
                     recordPromise.resolve(grades[id]);
-                }, Math.floor(Math.random()*2000, 1));
-
+                } else {
+                    serverInterface.getGradeById(id)
+                        .then(function(response){
+                            grades[id] = {
+                                name: response.name,
+                                course: response.course,
+                                grade: parseFloat(response.grade)
+                            };
+                            recordPromise.resolve(grades[id]);
+                        }, function(response){
+                            recordPromise.reject(response);
+                        }
+                    );
+                }
                 return recordPromise.promise;
             });
 
--- public/assets/js/services/server_interface.js
+++ public/assets/js/services/server_interface.js
@@ -1,5 +1,27 @@
-sgt.factory('serverInterface', [function(){
+sgt.factory('serverInterface', ['$q', '$http', function($q, $http){
     function ServerInterface(){
+
+        var baseUrl = 'api/';
+
+        this.getGradeById = function(id){
+
+            var result = $q.defer();
+            var url = baseUrl + 'grades/' + id;
+
+            $http.get(url).then(function(response){
+                if (response.hasOwnProperty('data') &&
+                    response.data.hasOwnProperty('success') &&
+                    response.data.success){
+                    result.resolve(response.data.data[0]);
+                } else {
+                    result.reject(response);
+                }
+            }, function(response){
+                result.reject(response);
+            });
+
+            return result.promise;
+        };
 
     }
 
