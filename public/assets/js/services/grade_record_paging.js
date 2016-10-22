fc8320bfbe7ab1f95219558f92ab4ecd6ccb4601
TimelikeClosure <thorist@gmail.com>
2016-10-21 17:49:54 -0700

    started hashing out gradeRecordPaging page update digest cycle

--- public/assets/js/services/grade_record_paging.js
+++ public/assets/js/services/grade_record_paging.js
@@ -24,6 +24,32 @@ sgt.factory('gradeRecordPaging', ['gradeCache', '$log', function(gradeCache, $lo
             }
         };
 
+        var pagedIdCache = {
+            1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            2: [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+        };
+
+        var visible = {
+            pages: [],
+            records: []
+        };
+
+        var cache = {
+            pages: {
+                current: [],
+                first: 1,
+                last: null,
+                selectable: [],
+                cached: []
+            },
+            ids: {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 8: 9, 9: 10, 10: 11, 11: 12, 12: 13, 13: 14, 14: 15, 15: 16, 16: 17, 17: 18},
+            records: {
+                perPage: 15,
+                total: null,
+                visible: []
+            }
+        };
+
         /**
          * @method setVisibleRecords
          * @param {int[]} pageList
@@ -34,8 +60,35 @@ sgt.factory('gradeRecordPaging', ['gradeCache', '$log', function(gradeCache, $lo
                 return last.concat(records.pages[current]);
             }, []);
             gradeCache.recordList(ids).then(function(visibleRecords){
-                records.visible = visibleRecords;
+                visible.records = visibleRecords;
             });
+        };
+
+        this.updateCurrentRecords = function(){
+            //  obtain references for current records from studentCache
+
+            //  update current record references
+        };
+
+        this.updateCurrentIds = function(){
+            //  validate current record id lists (check for gaps / duplicates on pages)
+
+            //  set record ids, obtaining incomplete / outdated pages from server
+
+            //  update current records
+        };
+
+        /**
+         * @method updateCurrentPages
+         * @param {int[]} pageList
+         * @summary Sets the currently displayed pages
+         */
+        this.updateCurrentPages = function(pageList){
+            //  validate current page list
+
+            //  set current page list
+
+            //  update current ids
         };
 
         /**
@@ -44,7 +97,7 @@ sgt.factory('gradeRecordPaging', ['gradeCache', '$log', function(gradeCache, $lo
          * @summary Get list of currently visible grade records with their details.
          */
         this.visibleRecords = function(){
-            return records.visible;
+            return visible.records;
         };
 
         this.setVisibleRecords([1]);
